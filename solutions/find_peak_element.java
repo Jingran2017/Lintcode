1832546546a2c4a598a8e3578d8aa83e2e13b132
Jingran2017 <chaoli@ChaotekiMacBook-Pro.local>
2017-05-03 02:00:34 -0700

    binary search

--- solutions/find_peak_element.java
+++ solutions/find_peak_element.java
@@ -1,5 +1,30 @@
 
 public class find_peak_element {
+    //2016刷题
+    public int findPeak(int[] A) {
+        // write your code here
+
+        if(A==null || A.length==0){
+            return -1;
+        }
+
+        int start=0;
+        int end=A.length-1;
+        while(start+1<end){
+            int mid=start+(end-start)/2;
+            if(A[mid]<A[mid+1]){
+                start=mid;
+            }
+            else if(A[mid]>A[mid+1]){
+                end=mid;
+            }
+        }
+
+        if(A[start]>A[end]){
+            return start;
+        }
+        else return end;
+    }
 
     //2017刷题
     public int findPeak(int[] A) {
