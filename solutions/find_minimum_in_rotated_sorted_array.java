bad917912d79ab26d36fd73c50908d50572d9137
Jingran2017 <chaoli@ChaotekiMacBook-Pro.local>
2017-05-03 01:04:10 -0700

    binary search

---
+++ solutions/find_minimum_in_rotated_sorted_array.java
@@ -0,0 +1,33 @@
+
+public class find_minimum_in_rotated_sorted_array {
+
+    public int findMin(int[] nums) {
+        // write your code here
+
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        int start=0;
+        int end=nums.length-1;
+
+        while (start+1<end) {
+            int mid=start+(end-start)/2;
+            if (nums[mid]>nums[end]) {
+                start=mid;
+            } else if (nums[mid]<nums[end]) {
+                end=mid;
+            }
+        }
+
+        if (nums[start]<nums[end]) {
+            return nums[start];
+        }
+
+        if (nums[end]<nums[start]) {
+            return nums[end];
+        }
+
+        return -1;
+    }
+}
