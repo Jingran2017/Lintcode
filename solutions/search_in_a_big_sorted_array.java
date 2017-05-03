fa8d693894ad3d3ec86b1d51172d526279bf6987
Jingran2017 <chaoli@ChaotekiMacBook-Pro.local>
2017-05-03 00:33:22 -0700

    binary search

--- solutions/search_in_a_big_sorted_array.java
+++ solutions/search_in_a_big_sorted_array.java
@@ -62,5 +62,37 @@ public class search_in_a_big_sorted_array {
     }
 
     //2017刷题
-    
+    public int searchBigSortedArray(ArrayReader reader, int target) {
+
+
+        int right=1;
+        while (reader.get(right-1)<target) {
+            right=right*2;
+        }
+
+        int start=0;
+        int end=right-1;
+
+        while (start+1<end) {
+            int mid=start+(end-start)/2;
+            if (reader.get(mid) == target) {
+                end=mid; // very important, 不能直接return mid。因为要求first position，那可能存在3 4 4 情况，mid等于后面那个4的情况，所以
+                        //  要end=mid，这样才会往前再次找mid。如果题目要求是last position，则应该是start=mid；
+            }  else if (reader.get(mid)>target) {
+                end=mid;
+            } else {
+                start=mid;
+            }
+        }
+
+        if (reader.get(start) == target) {
+            return start;
+        }
+
+        if (reader.get(end) == target) {
+            return end;
+        }
+
+        return -1;
+    }
 }
