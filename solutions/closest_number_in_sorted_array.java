a65a39d422d51a6f6e7540a96aedb6d9d589e749
Jingran2017 <chaoli@ChaotekiMacBook-Pro.local>
2017-05-02 23:30:43 -0700

    binary search

---
+++ solutions/closest_number_in_sorted_array.java
@@ -0,0 +1,38 @@
+/**
+ * Created by chaoli on 2017/5/2.
+ */
+public class closest_number_in_sorted_array {
+    public int closestNumber(int[] A, int target) {
+        // Write your code here
+
+        if ( A == null || A.length ==0) {
+            return -1;
+        }
+
+        int start=0;
+        int end=A.length-1;
+
+        while (start+1<end) {
+            int mid=start+(end-start)/2;
+            if (A[mid] == target) {
+                return mid;
+            } else if (A[mid]<target) {
+                start=mid;
+            } else if (A[mid]>target) {
+                end=mid;
+            }
+        }
+
+        if (target<=A[start]) {
+            return start;
+        } else if (target>=A[end]) {
+            return end;
+        } else if (target-A[start]>A[end]-target) {
+            return end;
+        } else if (target-A[start]<=A[end]-target) {
+            return start;
+        }
+
+        return -1;
+    }
+}
